9a51bfd39e2ac0154097dc18bf5a45685a6145eb
Steve <stevewu24@gmail.com>
2022-10-23 11:16:52 -0700

    added carousel

--- frontend/src/components/pages/Home/Home.jsx
+++ frontend/src/components/pages/Home/Home.jsx
@@ -1,18 +1,50 @@
+/* eslint-disable no-undef */
 import React from 'react';
 import styled from 'styled-components';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
+const { Carousel } = require('react-responsive-carousel');
 
 const StyledHome = styled.div`
-  margin: 5rem;
+  @media (min-width: 300px) {
+    margin: 2rem;
+  }
+  @media(max-width: 300px) {
+    margin: 0rem;
+  }
 `;
 
-class Home extends React.PureComponent {
+// carousel library found at https://github.com/leandrowd/react-responsive-carousel
+
+export default class Home extends React.PureComponent {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
   render() {
     return (
       <StyledHome>
-        <h1>Home Page</h1>
+        <Carousel
+          showThumbs={false}
+          swipeable
+          emulateTouch
+          infiniteLoop
+          showArrows={false}
+          autoPlay
+          stopOnHover
+          dynamicHeight
+        >
+          <div>
+            <img src="https://cdn.pixabay.com/photo/2018/08/14/13/23/ocean-3605547__480.jpg" alt="img" />
+            <p>Item 1</p>
+          </div>
+          <div>
+            <img src="https://cdn.pixabay.com/photo/2018/08/14/13/23/ocean-3605547__480.jpg" alt="img" />
+            <p>Item 2</p>
+          </div>
+        </Carousel>
       </StyledHome>
     );
   }
 }
-
-export default Home;
